530c52dcf65a429b43a197d1c27830c892f60418
stappost <emanuele.scarpantoni@gmail.com>
2024-02-18 10:09:24 +0100

    create table in db

---
+++ database/migrations/2024_02_18_090043_create_comics_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 255);
+            $table->text('description');
+            $table->string('thumb', 255);
+            $table->string('series', 50);
+            $table->string('type', 50);
+            $table->string('price');
+            $table->string('sale_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comics');
+    }
+};
