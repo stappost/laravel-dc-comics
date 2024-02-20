9a99269786c6756171c77a1a60efff29f9587752
stappost <emanuele.scarpantoni@gmail.com>
2024-02-20 15:05:06 +0100

    add validation

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\comic;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class ComicController extends Controller
 {
@@ -36,7 +37,7 @@ class ComicController extends Controller
      */
     public function store(Request $request)
     {
-        $form_data = $request->all();
+        $form_data = $this->validation($request->all());
 
         $comic = new Comic();
         $comic->title = $form_data['title'];
@@ -66,10 +67,11 @@ class ComicController extends Controller
      */
     public function show(comic $comic)
     {
-        $comic['arts'] = explode(',', $comic['arts']);
-        $comic['writers'] = explode(',', $comic['writers']);
         // $comic['writers'] = json_decode($comic['writers']);
         // $comic['arts'] = json_decode($comic['arts']);
+        $comic['arts'] = explode(',', $comic['arts']);
+        $comic['writers'] = explode(',', $comic['writers']);
+
         return view('details', compact('comic'));
     }
 
@@ -93,7 +95,7 @@ class ComicController extends Controller
      */
     public function update(Request $request, comic $comic)
     {
-        $form_data = $request->all();
+        $form_data = $this->validation($request->all());
 
         $comic->title = $form_data['title'];
         $comic->description = $form_data['description'];
@@ -102,11 +104,11 @@ class ComicController extends Controller
         $comic->type = $form_data['type'];
         $comic->price = $form_data['price'];
         $comic->sale_date = $form_data['sale_date'];
-        $form_data['writers'] = json_encode($form_data['writers']);
         $comic->writers = $form_data['writers'];
-        $form_data['arts'] = json_encode($form_data['arts']);
         $comic->arts = $form_data['arts'];
-
+        
+        $comic['arts'] = json_encode($comic['arts']);
+        $comic['writers'] = json_encode($comic['writers']);
         $comic->update();
         
         return redirect()->route('comics.show', ['comic' => $comic]);
@@ -123,5 +125,39 @@ class ComicController extends Controller
         $comic->delete();
 
         return redirect()->route('comics.store');
+    }
+
+    private function validation($data){
+        
+        $validator = Validator::make(
+            $data,
+            [
+                'title' => 'required|max:255' ,
+                'description' => 'required',
+                'thumb' => 'required',
+                'series' => 'required|max:50',
+                'type' => 'required|max:50',
+                'price' => 'required|max:15',
+                'sale_date' => 'required|max:15',
+                'writers' => '',
+                'arts' => ''
+            ],
+            [
+                'title.required' => 'Il titolo è obbligatorio',
+                'title.max' => 'Il titolo deve essere al massimo 255 caratteri',
+                'description.required' => 'La descrizione è obbligatoria',
+                'thumb.required' => 'Il link dell\'immagine è obbligatorio',
+                'series.required' => 'La serie è obbligatoria',
+                'series.max' => 'La serie deve essere al massimo 50 caratteri',
+                'type.require' => 'Il tipo è obbligatorio',
+                'type.max' => 'il tipo deve essere al massimo 50 caratteri',
+                'price.require' => 'Il prezzo è obbligatorio',
+                'price.max' => 'il prezzo deve essere al massimo 15 caratteri',
+                'sale_date.require' => 'la data di vendita è obbligatoria',
+                'sale_date.max' => 'la data di vendita deve essere al massimo 15 caratteri'
+            ]
+        )->validate();
+
+        return $validator;
     }
 }
--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -5,6 +5,11 @@
         <div class="row">
             <div class="col-12 my-4">
                 <h3>Aggiungi un nuovo fumetto</h3>
+                @if ($errors->any())
+                    @foreach ($errors->all() as $error)
+                        <div>{{ $error }}</div>
+                    @endforeach
+                @endif
                 <form action="{{ route('comics.store') }}" method="post">
                     @csrf
 
